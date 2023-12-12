40292fa92132fa7393ba373c58db5b7e49fb6aaf
xuanviet73 <2012393@dlu.edu.vn>
2023-12-12 18:35:50 +0700

    Blog-Website

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,10 +1,5 @@
 let firebaseConfig = {
-    apiKey: "AIzaSyBtsSlaV6XvskJCJ33T6VVK_QYOtuanZRE",
-    authDomain: "blogging-website-bd418.firebaseapp.com",
-    projectId: "blogging-website-bd418",
-    storageBucket: "blogging-website-bd418.appspot.com",
-    messagingSenderId: "304307877981",
-    appId: "1:304307877981:web:9cba32c24b6da806ba75b4",
+    //Nhập thông tin đăng nhập firebase của bạn
   };
   
   // Correct the identifier to 'firebase'
